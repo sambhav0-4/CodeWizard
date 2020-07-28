eadab5ee3907dd175e8e6142acadb8c8ec0142d2
sambhav0-4 <62285367+sambhav0-4@users.noreply.github.com>
2020-07-28 14:53:01 +0530

    New Commit

---
+++ Static/js/script.js
@@ -0,0 +1,65 @@
+$(document).ready(function(){
+    console.log("loaded");
+    $.material.init();
+    $(document).on("submit", "#register-form", function(e){
+        e.preventDefault();
+
+        var form = $("#register-form").serialize();
+        $.ajax({
+            url: '/post',
+            type: 'POST',
+            data: form,
+            success: function(response){
+                console.log(response);
+            }
+        });
+    });
+
+    $(document).on("submit", "#login-form", function(e){
+        e.preventDefault();
+
+        var form = $(this).serialize();
+        $.ajax({
+            url: '/check-login',
+            type: 'POST',
+            data: form,
+            success: function(res){
+                if (res == "error"){
+                    alert("could not login");
+                }else{
+                    console.log("Logged in as: ", res);
+                    window.location.href = "/";
+                }
+            }
+        });
+    });
+
+    $(document).on("click","#logout-link", function(e){
+        e.preventDefault();
+        $.ajax({
+        url: '/logout',
+        type: 'GET',
+        success: function(res){
+            if(res == 'success'){
+                window.location.href = '/login';
+            }else{
+                alert("Something went wrong! ");
+            }
+        }
+        });
+    });
+
+    $(document).on("submit", "#post-activity", function(e){
+        e.preventDefault();
+        form = $(this).serialize();
+        $.ajax({
+            url: '/post-activity',
+            type: 'POST',
+            data: form,
+            success: function(res){
+                console.log(res);
+            }
+
+        });
+    });
+});
---
+++ controller.py
@@ -0,0 +1,89 @@
+import web
+from Models import RegisterModel
+from Models import LoginModel
+from Models import Posts
+
+web.config.debug = False
+
+urls = (
+    "/", "Home",
+    "/register", "Register",
+    "/login", "Login",
+    "/logout", "Logout",
+    "/post", "PostRegistration",
+    "/check-login", "CheckLogin",
+    "/post-activity", "PostActivity"
+)
+
+app = web.application(urls, globals())
+session = web.session.Session(app, web.session.DiskStore("sessions"), initializer={"user": None})
+session_data = session._initializer
+
+render = web.template.render("Views/Templates", base="MainLayout", globals={'session': session_data, 'current_user': session_data["user"]})
+
+
+# Classes/routes
+
+class Home:
+    def GET(self):
+        data = type('obj', (object,), {"username": "sam1", "password": "avocado1"})
+
+        login = LoginModel.LoginModel()
+        isCorrect = login.check_user(data)
+
+        if isCorrect:
+            session_data["user"] = isCorrect
+
+        return render.Home()
+
+
+class Register:
+    def GET(self):
+        return render.Register()
+
+
+class Login:
+    def GET(self):
+        return render.Login()
+
+
+class PostRegistration:
+    def POST(self):
+        data = web.input()
+        reg_model = RegisterModel.RegisterModel()
+        reg_model.insert_user(data)
+        return data.username
+
+
+class CheckLogin:
+    def POST(self):
+        data = web.input()
+        login = LoginModel.LoginModel()
+        isCorrect = login.check_user(data)
+
+        if isCorrect:
+            session_data["user"] =isCorrect
+            return isCorrect
+
+        return "error"
+
+
+class PostActivity:
+    def POST(self):
+        data = web.input()
+        data.username = session_data["user"]["username"]
+        post_model = Posts.Posts()
+        post_model.insert_post(data)
+        return "success"
+
+
+class Logout:
+    def GET(self):
+        session['user'] = None
+        session_data['user'] = None
+        session.kill()
+        return "success"
+
+
+if __name__ == "__main__":
+    app.run()
