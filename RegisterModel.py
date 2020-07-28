054b6eacb81b551139e4fd590c872e750a773656
sambhav0-4 <62285367+sambhav0-4@users.noreply.github.com>
2020-07-28 12:20:03 +0530

    Add files via upload

---
+++ RegisterModel.py
@@ -0,0 +1,21 @@
+import pymongo
+from pymongo import MongoClient
+import bcrypt
+
+
+class RegisterModel:
+    def __init__(self):
+        self.client = MongoClient()
+        self.db = self.client.CodeWizard
+        self.Users = self.db.users
+
+    def insert_user(self, data):
+
+        hashed = bcrypt.hashpw(data.password.encode(), bcrypt.gensalt())
+
+        id = self.Users.insert({"username": data.username, "name": data.name, "password": hashed, "email": data.email })
+        print("Uid is: ", id)
+        myuser = self.Users.find_one({"username": data.username})
+
+        if bcrypt.checkpw("avocado1".encode(), myuser["password"]):
+            print("this matches")
